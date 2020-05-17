0db9f3c4672d5dcca233de39ccfc1f0c2a95ca77
fleatama <fleatama@gmail.com>
2020-05-17 14:06:13 +0900

    change model user and article and comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   # dependent: :destroy は user を削除した際に articles もまとめて削除するオプション
   has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
